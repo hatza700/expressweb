2318d2cbee669ae33482fd10d6b426e1b01e1d39
huyenhs <thuocla2011@gmail.com>
2016-03-10 09:34:25 +0700

    Added files via upload

---
+++ CMStTrafficSafety/StoredProcedures/pCMStTrafficSafetyTTrafficSafety_31_type_menuAdd.sql
@@ -0,0 +1,31 @@
+﻿if exists (select * from sysobjects where id = object_id(N'pCMStTrafficSafetyTTrafficSafety_31_type_menuAdd') and sysstat & 0xf = 4) drop procedure pCMStTrafficSafetyTTrafficSafety_31_type_menuAdd 
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Creates a new record in the [dbo].[tTrafficSafety_31_type_menu] table.
+CREATE PROCEDURE pCMStTrafficSafetyTTrafficSafety_31_type_menuAdd
+    @p_TenKieuMenu nvarchar(300),
+    @p_GhiChu nvarchar(550),
+    @p_id_out int output
+AS
+BEGIN
+    INSERT
+    INTO [dbo].[tTrafficSafety_31_type_menu]
+        (
+            [TenKieuMenu],
+            [GhiChu]
+        )
+    VALUES
+        (
+             @p_TenKieuMenu,
+             @p_GhiChu
+        )
+
+    SET @p_id_out = SCOPE_IDENTITY()
+
+END
+
---
+++ CMStTrafficSafety/StoredProcedures/pCMStTrafficSafetyTTrafficSafety_31_type_menuDelete.sql
@@ -0,0 +1,17 @@
+﻿if exists (select * from sysobjects where id = object_id(N'pCMStTrafficSafetyTTrafficSafety_31_type_menuDelete') and sysstat & 0xf = 4) drop procedure pCMStTrafficSafetyTTrafficSafety_31_type_menuDelete 
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- Deletes a record from the [dbo].[tTrafficSafety_31_type_menu] table.
+CREATE PROCEDURE pCMStTrafficSafetyTTrafficSafety_31_type_menuDelete
+        @pk_id int
+AS
+BEGIN
+    DELETE [dbo].[tTrafficSafety_31_type_menu]
+    WHERE [id] = @pk_id
+END
+
